2fc0a7d557f5006d023cb5ac1e53f86a435b63a4
Marc Hanley <marcjameshanley@gmail.com>
2017-08-18 11:37:31 +0930

    Refactor file parsing function
    	* For-loop for file-reading function now calls function
    	* Did this so that its easier to test file-reading

--- lib/file_parser/file_parser.py
+++ lib/file_parser/file_parser.py
@@ -44,44 +44,50 @@ class file_parser(object):
 
     def preprocess_file(self):
         #read in file contents
-        i = 0
+        index = 0
         sentence = ""
         delim_exp = r'.+[!?.].*'
-        #Strip guts into function
-        #Test THAT function instead!
-        for line in self.file:
-            #Remove leading/trailing whitespace and newlines
-            line = line.strip()
-            if line == '':
-                self.sentences.append("\n")
-                continue
-            if re.match(delim_exp, line) is not None:
-                #line = re.split('[!?.]', line)
-                line = re.findall(delim_exp, line, re.DOTALL)
 
-                if i != 0:
-                    #Append second half of sentence to previous sentence
-                    line[0] = sentence + "\n" + line[0]
-                self.sentences.append(line[0])
-                #...why is this if branch in here again?
-                if len(line) > 1:
-                    #Process all sentences and append to list
-                    for w in line:
-                        if w == line[0]: #already did this
-                            continue
-                        elif w == line[len(line) - 1]:
-                            #New uncomplete sentence for final list item
-                            sentence = w
-                        else:
-                            self.sentences.append(w)
-            else: #full line, add to previous sentence
-                #sentence += line
-                sentence = sentence + "\n" + line
-            i = i + 1
-        #DEBUG
-        #for line in self.sentences:
-        #    print " Line: " + line
+        #For each line, apply processing to line and
+        #add to sentence list
+        for line in self.file:
+            sentence = self.process_line(index, line, sentence, delim_exp)
+            index = index + 1
+
         return self.sentences
+
+    def process_line(self, index, line, sentence, delim_exp):
+        #Remove leading/trailing whitespace and newlines
+        line = line.strip()
+        if line == '\n':
+            self.sentences.append(line)
+            return sentence
+        if re.match(delim_exp, line) is not None:
+            #line = re.split('[!?.]', line)
+            line = re.findall(delim_exp, line, re.DOTALL)
+
+            if index != 0:
+                #Append second half of sentence to previous sentence
+                line[0] = sentence + "\n" + line[0]
+            self.sentences.append(line[0])
+            #...why is this if branch in here again?
+            if len(line) > 1:
+                #Process all sentences and append to list
+                for w in line:
+                    if w == line[0]: #already did this
+                        continue
+                    elif w == line[len(line) - 1]:
+                        #New uncomplete sentence for final list item
+                        sentence = w
+                    else:
+                        self.sentences.append(w)
+        else: #full line, add to previous sentence
+            #sentence += line
+            sentence = sentence + "\n" + line
+            self.sentences.append(sentence)
+            sentence = ""
+            return sentence
+        return sentence
 
     #Set ruleset dictionary
     def set_ruleset(self, ruleset):
