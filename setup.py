dacd49f13d8d953cf300360f2002b8fab3481364
Marc Hanley <marcjameshanley@gmail.com>
2017-09-15 14:17:13 +0930

    Adjust setup variables

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
-from setuptools import setup
+from distutils.core import setup
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='Strunk',
     version='1.0',
-    description='Command-line tool for validating text files with regular expressions',
+    description='Command-line tool for validating and re-writing text files with regular expressions',
     author='Marc Hanley',
     author_email='marcjameshanley@gmail.com',
-    packages=['lib'])
+    licence='MIT',
+    keywords='validate text regular expressions',
+    long_description=read('README.md'),
+    packages=['strunk', 'rulesets', 'strunk/file_parser', 'strunk/rule_parser', 'strunk/rule_applier', 'tests'])
